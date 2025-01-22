002ea5476895c0c79436a97aba14889792e99b96
Faheem Hassan <faheemh127@gmail.com>
2025-01-22 18:54:44 +0500

    Add Some headings and paragraph to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1 className="text-2xl">Finegap Deployment Project</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quod nam, ab commodi aliquam suscipit, sed voluptatibus sit ipsam vel debitis quia ad architecto provident esse nulla eveniet impedit! Mollitia!</p>
+      <p>Code is cheap show me the product</p>
         <Image
           className="dark:invert"
           src="/next.svg"
